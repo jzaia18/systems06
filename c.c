747e9712f85cfc162fea37cedaf0c1550cb3277c
Jake Zaia <jzaia@stuy.edu>
2017-10-13 15:33:47 -0400

    Added method headers

---
+++ c.c
@@ -0,0 +1,16 @@
+// Jake Zaia
+// Systems p10
+// 2017-10-13
+// Work06
+
+struct node { int i; struct node *next };
+
+void print_list(struct node *);
+
+struct node * insert_front(Struct node *, int);
+
+struct node * free_list(struct node *);
+
+int main () {
+  
+}
